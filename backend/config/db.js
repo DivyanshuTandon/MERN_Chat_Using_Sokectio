5d0064450f8bb36ec9c1d01c1e1076b1172abfc6
Divyanshu <109156916+DivyanshuTandon@users.noreply.github.com>
2023-11-23 15:45:32 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,7 +4,6 @@ const colors = require("colors");
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
-useNewUrlParser: true, useUnifiedTopology: true 
 
       
     });
